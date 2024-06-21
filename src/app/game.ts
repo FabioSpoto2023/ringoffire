42402ab738b87088fa8cc5f610aab936b88e6ed6
FabioSpoto2023 <FabioSpoto2023@gmail.com>
2024-06-21 09:20:38 +0200

    added game field

---
+++ src/app/game.ts
@@ -0,0 +1,8 @@
+export class Game {
+    public player: string[] = [];
+    public stack: string[] = [];
+    public playerCards: string[] = [];
+    public currentPlayer: number = 0;
+
+    constructor() { }
+}
